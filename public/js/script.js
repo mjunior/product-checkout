b61799c93edabf936c1ca5c0b1fcccea04ce349d
mjunior <mauriciojs.junior@gmail.com>
2019-04-28 18:46:41 -0300

    Get values from api

--- public/js/script.js
+++ public/js/script.js
@@ -86,9 +86,117 @@ function () {
   }]);
 
   return ActionButtons;
+}(); //PathParamRouter
+
+
+var PathParam =
+/*#__PURE__*/
+function () {
+  function PathParam() {
+    _classCallCheck(this, PathParam);
+  }
+
+  _createClass(PathParam, null, [{
+    key: "params",
+    value: function params() {
+      var array = window.location.pathname.split('/').filter(function (item) {
+        return item !== "";
+      });
+      var result = {};
+
+      for (var i = 0; i < array.length; i = i + 2) {
+        result[array[i]] = array[i + 1];
+      }
+
+      return result;
+    }
+  }]);
+
+  return PathParam;
+}();
+
+var PrintHtmlPage =
+/*#__PURE__*/
+function () {
+  function PrintHtmlPage(id, value) {
+    var opts = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
+    _classCallCheck(this, PrintHtmlPage);
+
+    this.value = value;
+    this.opts = opts;
+    this.element = document.querySelector("#".concat(id));
+  }
+
+  _createClass(PrintHtmlPage, [{
+    key: "show",
+    value: function show() {
+      this.element.innerText = this._formatMoney(this.value);
+      if (this.opts.accent) this.element.classList.add("accent-text");
+      if (this.opts.bold) this.element.classList.add("bold-text");
+    }
+  }, {
+    key: "_formatMoney",
+    value: function _formatMoney(amount) {
+      var decimalCount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+      var decimal = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : ",";
+      var thousands = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : ".";
+
+      try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+        var negativeSign = amount < 0 ? "-" : "";
+        var i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        var j = i.length > 3 ? i.length % 3 : 0;
+        return "R$ ".concat(negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : ""));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }]);
+
+  return PrintHtmlPage;
+}(); //Load product checkout 
+
+
+var LoadCheckoutPage =
+/*#__PURE__*/
+function () {
+  function LoadCheckoutPage(checkout_id) {
+    _classCallCheck(this, LoadCheckoutPage);
+
+    this.checkout_id = checkout_id;
+    this.perform();
+  }
+
+  _createClass(LoadCheckoutPage, [{
+    key: "perform",
+    value: function perform() {
+      fetch("/api/checkouts/".concat(this.checkout_id)).then(function (result) {
+        return result.json();
+      }).then(function (result) {
+        var fields = {
+          shippingPrice: result.checkout.shippingPrice,
+          totalPrice: result.checkout.totalPrice,
+          price: result.product.price,
+          image: result.product.image
+        };
+        new PrintHtmlPage('amount-total', fields.totalPrice, {
+          accent: true,
+          bold: true
+        }).show();
+        new PrintHtmlPage('amount-shipping', fields.shippingPrice).show();
+        new PrintHtmlPage('amount-original', fields.price).show();
+        console.log(fields);
+      });
+    }
+  }]);
+
+  return LoadCheckoutPage;
 }();
 
 $(document).ready(function () {
+  new LoadCheckoutPage(PathParam.params().checkout);
   new ActionButtons();
 });
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,79 @@ class ActionButtons{
   }
 }
 
+//PathParamRouter
+class PathParam {
+  static params() {
+    const array = window.location.pathname.split('/').filter(item => item !== "")
+    const result = {}
+    for (let i = 0; i < array.length; i = i + 2) {
+      result[array[i]] = array[i + 1];
+    }
+    return result;
+  }
+}
+
+class PrintHtmlPage{
+  constructor(id, value, opts = {}){
+    this.value = value;
+    this.opts = opts
+    this.element = document.querySelector(`#${id}`)
+  }
+
+  show(){
+    this.element.innerText = this._formatMoney(this.value)
+    if(this.opts.accent)
+      this.element.classList.add("accent-text");
+      
+    if (this.opts.bold) 
+      this.element.classList.add("bold-text");
+    
+  }
+
+ _formatMoney(amount, decimalCount = 2, decimal = ",", thousands = ".") {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+    const negativeSign = amount < 0 ? "-" : "";
+
+    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+    let j = (i.length > 3) ? i.length % 3 : 0;
+
+    return `R$ ${negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "")}`;
+  } catch (e) {
+    console.log(e)
+  }
+}
+}
+
+//Load product checkout 
+class LoadCheckoutPage{
+  constructor(checkout_id){
+    this.checkout_id = checkout_id;
+    this.perform();
+  }
+
+  perform(){
+    fetch(`/api/checkouts/${this.checkout_id}`)
+      .then((result) => result.json())
+      .then((result) => {
+        const fields = {
+          shippingPrice: result.checkout.shippingPrice,
+          totalPrice: result.checkout.totalPrice,
+          price: result.product.price,
+          image: result.product.image
+        }
+
+        new PrintHtmlPage('amount-total', fields.totalPrice, { accent: true, bold: true }).show();
+        new PrintHtmlPage('amount-shipping', fields.shippingPrice).show();
+        new PrintHtmlPage('amount-original', fields.price).show();
+        console.log(fields);
+      })
+  }
+}
+
 $(document).ready(() =>{
+  new LoadCheckoutPage(PathParam.params().checkout);
   new ActionButtons();
 })
